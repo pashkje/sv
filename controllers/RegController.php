3d6c6631ceb03957170815e337a99e66f22823ff
Щербич <pashkje@gmail.com>
2016-10-26 16:53:15 +0400

    install project

---
+++ controllers/RegController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bitrix
+ * Date: 21.10.2016
+ * Time: 2:43
+ */
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+
+class RegController extends Controller
+{
+    public function ActionIndex()
+    {
+        echo "daa";
+    }
+
+}
